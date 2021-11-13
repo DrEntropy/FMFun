6b16901d541e1a441951eb4b7a30721261c92403
Dr Entropy <rjljr2@gmail.com>
2021-11-13 15:43:35 -0700

    Added a simple ADSR and removed tailoff

--- Source/FMWave.h
+++ Source/FMWave.h
@@ -20,14 +20,23 @@ struct FMSound   : public juce::SynthesiserSound
 //==============================================================================
 struct FMVoice   : public juce::SynthesiserVoice
 {
-    FMVoice(juce::AudioProcessorValueTreeState& apvts):apvts(apvts) {}
+    FMVoice(juce::AudioProcessorValueTreeState& apvts):apvts(apvts) {
+        
+   
+    }
     
     void setCurrentPlaybackSampleRate (double newRate) override {
         // not sure this is the best place for this .
         // hardwired 50 ms transition rate.
         
         if(newRate>0)
+        {
           s_mI.reset(newRate,0.050f);
+            ampEnv.setSampleRate(newRate);
+            modEnv.setSampleRate(newRate);
+         ampEnv.setParameters(juce::ADSR::Parameters(.01f,.1f,0.0f,.1f));
+         modEnv.setParameters(juce::ADSR::Parameters(.1f,.1f,1.0f,.1f));
+        }
         // call super
         juce::SynthesiserVoice::setCurrentPlaybackSampleRate(newRate);
     }
@@ -42,24 +51,27 @@ struct FMVoice   : public juce::SynthesiserVoice
     {
         currentAngle = 0.0;
         level = velocity * 0.15;
-        tailOff = 0.0;
+//        tailOff = 0.0;
       
 
         auto cyclesPerSecond = juce::MidiMessage::getMidiNoteInHertz (midiNoteNumber);
         auto cyclesPerSample = cyclesPerSecond / getSampleRate();
 
         angleDelta = cyclesPerSample * 2.0 * juce::MathConstants<double>::pi;
+        ampEnv.noteOn();
     }
 
     void stopNote (float /*velocity*/, bool allowTailOff) override
     {
         if (allowTailOff)
         {
-            if (tailOff == 0.0)
-                tailOff = 1.0;
+            ampEnv.noteOff();
+//            if (tailOff == 0.0)
+//                tailOff = 1.0;
         }
         else
         {
+            ampEnv.reset();
             clearCurrentNote();
             angleDelta = 0.0;
         }
@@ -79,13 +91,13 @@ struct FMVoice   : public juce::SynthesiserVoice
             s_mI.setTargetValue(newmI);
             float mI;
             
-            if (tailOff > 0.0) // [7]
+            if (ampEnv.isActive()) // [7]
             {
                
                 while (--numSamples >= 0)
                 {
                     mI = s_mI.getNextValue();
-                    auto currentSample = (float) (std::sin (currentAngle + mI*std::sin(currentAngle)) * level * tailOff);
+                    auto currentSample = (float) (std::sin (currentAngle + mI*std::sin(currentAngle)) * level * ampEnv.getNextSample());
 
                     for (auto i = outputBuffer.getNumChannels(); --i >= 0;)
                         outputBuffer.addSample (i, startSample, currentSample);
@@ -93,41 +105,48 @@ struct FMVoice   : public juce::SynthesiserVoice
                     currentAngle += angleDelta;
                     ++startSample;
 
-                    tailOff *= 0.99; // [8]
+              //      tailOff *= 0.99; // [8]
 
-                    if (tailOff <= 0.005)
-                    {
-                        clearCurrentNote(); // [9]
-
-                        angleDelta = 0.0;
-                        break;
-                    }
+//                    if (tailOff <= 0.005)
+//                    {
+//                        clearCurrentNote(); // [9]
+//
+//                        angleDelta = 0.0;
+//                        break;
+//                    }
                 }
             }
             else
             {
-                while (--numSamples >= 0) // [6]
-                {
-                    mI = s_mI.getNextValue();
-                    auto currentSample = (float) (std::sin (currentAngle+ mI*std::sin(currentAngle)) * level);
+                clearCurrentNote(); // [9]
 
-                    for (auto i = outputBuffer.getNumChannels(); --i >= 0;)
-                        outputBuffer.addSample (i, startSample, currentSample);
-
-                    currentAngle += angleDelta;
-                    ++startSample;
-                }
+                angleDelta = 0.0;
+//                while (--numSamples >= 0) // [6]
+//                {
+//                    mI = s_mI.getNextValue();
+//                    auto currentSample = (float) (std::sin (currentAngle+ mI*std::sin(currentAngle)) * level);
+//
+//                    for (auto i = outputBuffer.getNumChannels(); --i >= 0;)
+//                        outputBuffer.addSample (i, startSample, currentSample);
+//
+//                    currentAngle += angleDelta;
+//                    ++startSample;
+//                }
             }
         }
     }
 
 private:
-    double currentAngle = 0.0, angleDelta = 0.0, level = 0.0, tailOff = 0.0;
+    double currentAngle = 0.0, angleDelta = 0.0, level = 0.0;
+//    double tailOff = 0.0;
     
     juce::AudioProcessorValueTreeState& apvts;
     
     //smothing variables starts at zero
     juce::SmoothedValue<float> s_mI{0.f};
+    
+    juce::ADSR ampEnv{};
+    juce::ADSR modEnv{};
 };
 
 
